76d148c54ddc453b121ff4cd578637fa6ad84e47
Yomar Miranda <mirandayomar@gmail.com>
2018-08-24 23:05:10 -0500

    Groupings of the objects.

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -8,21 +8,48 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class DataComponent {
   formData: FormGroup;
+  user: Object = {
+    fullname: {
+      name: 'Yomar',
+      lastname: 'Miranda'
+    },
+    email: 'yomar@gmail.com'
+  };
 
   constructor() {
+    console.log(this.user);
+
     this.formData = new FormGroup({
-      'name': new FormControl('', [
-                                    Validators.required,
-                                    Validators.minLength(4)
-                                  ]),
-      'lastname': new FormControl('', Validators.required),
+      'fullname': new FormGroup({
+                      'name': new FormControl('', [
+                                                    Validators.required,
+                                                    Validators.minLength(4)
+                                                  ]
+                                ),
+                      'lastname': new FormControl('', Validators.required)
+      }),
       'email': new FormControl('', [
                                       Validators.required,
                                       // Validators.email,
                                       Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
                                     ]
                               )
-    });
+      });
+
+    /*this.formData = new FormGroup({
+    'name': new FormControl('', [
+                                  Validators.required,
+                                  Validators.minLength(4)
+                                ]
+                            ),
+    'lastname': new FormControl('', Validators.required),
+    'email': new FormControl('', [
+                                    Validators.required,
+                                    // Validators.email,
+                                    Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+                                  ]
+                            )
+    });*/
   }
 
   saveChanges() {
