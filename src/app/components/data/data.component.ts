8dfcea033d9ed297ef6d155ff2cbea3720887357
Yomar Miranda <mirandayomar@gmail.com>
2018-08-24 21:58:45 -0500

    Linking inputs to properties of FormGroup.

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -6,19 +6,25 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent{
+export class DataComponent {
   formData: FormGroup;
 
   constructor() {
     this.formData = new FormGroup({
-      'name': new FormControl('Yomar'),
-      'lastname': new FormControl(),
-      'email': new FormControl()
+      'name': new FormControl('', Validators.required),
+      'lastname': new FormControl('', Validators.required),
+      'email': new FormControl('', [
+                                      Validators.required,
+                                      Validators.email,
+                                      Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+                                    ]
+                              )
     });
   }
 
   saveChanges() {
     console.log(this.formData.value);
+    console.log(this.formData);
   }
 
 }
