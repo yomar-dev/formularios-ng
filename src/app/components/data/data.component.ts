8957f2a3c1a0abf46eb663791070dd2b7e069301
Yomar Miranda <mirandayomar@gmail.com>
2018-08-24 20:58:30 -0500

    Data: Base form.

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent{
+  formData: FormGroup;
 
-  constructor() { }
+  constructor() {
+    this.formData = new FormGroup({
+      'name': new FormControl('Yomar'),
+      'lastname': new FormControl(),
+      'email': new FormControl()
+    });
+  }
 
-  ngOnInit() {
+  saveChanges() {
+    console.log(this.formData.value);
   }
 
 }
